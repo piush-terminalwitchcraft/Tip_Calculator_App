f9db1332aead75cd3cc180b38b6d7997dba68e46
Piush <piushpaul.16@gmail.com>
2021-07-11 03:48:21 +0530

    Added manual selection

--- app/src/main/java/com/example/tipcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/tipcalculatorapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.tipcalculatorapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.slider.Slider;
@@ -21,9 +24,9 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     private TextInputLayout textInputLayout;
     private TextInputEditText tet;
-    EditText editText;
     Slider slider;
     TextView tt;
+    EditText tt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +37,47 @@ public class MainActivity extends AppCompatActivity {
         tt = findViewById(R.id.textView);
         tet = findViewById(R.id.amount);
         button = findViewById(R.id.button);
+        tt2 = findViewById(R.id.tt2);
+        tt2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                slider.setValue(0.00f);
+                tt2.setSelection(tt2.getText().length());
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString().length() != 0){
+                float k = Float.parseFloat(s.toString());
+                    if(k>=0 && k<=10){
+                        slider.setValue(k);
+                    }
+                }
+                else if(s.toString().length()==2){
+                    tt2.getText().clear();
+                }
+            }
+        });
+        slider.addOnChangeListener(new Slider.OnChangeListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                //Use the value
+                String f = String.valueOf(slider.getValue());
+//                String val = String.valueOf(slider.getValue());
+                if(f.length()>3){
+                f = String.format("%.2f",slider.getValue());
 
+                }
+                tt2.setText(f);
+            }
+        });
         addListenerOnButton();
 
 
     }
-
         public void addListenerOnButton() {
 
         final Context context = this;
@@ -54,15 +92,15 @@ public class MainActivity extends AppCompatActivity {
                 String TipPercentage = String.valueOf(slider.getValue());
                 float billAmt =Float.parseFloat(BillAmount);
                 float b = Float.parseFloat(TipPercentage);
-                float tipAmt = billAmt*b/100;
-                String TipAmount = Float.toString(tipAmt);
+                float tipAmt = billAmt*b/100.00f;
+                @SuppressLint("DefaultLocale") String TipAmount = String.format("%.2f",tipAmt);
                 Intent intent = new Intent(context, final_screen.class);
                 intent.putExtra("BillAmt",BillAmount);
                 intent.putExtra("TipAmt",TipAmount);
                 startActivity(intent);
                 overridePendingTransition(R.anim.animate, R.anim.animeleft);
             }
-
         });
+
     }
 }
